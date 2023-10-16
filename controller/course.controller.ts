ffbaf82e3457d0ea94a15388968538192e51761d
AbtahiHasan <anikidpersonal@gmail.com>
2023-10-16 15:32:55 +0600

    image upload and update for course

--- controller/course.controller.ts
+++ controller/course.controller.ts
@@ -3,11 +3,23 @@ import catchAsyncError from "../middleware/catchAsyncError"
 import ErrorHandler from "../utils/ErrorHandler"
 import { createCourse } from "../services/course.service"
 import courseModel from "../models/course.model"
+import { v2 as cloudinary } from "cloudinary"
 
 
 const uploadCourse = catchAsyncError(async (req: Request, res: Response, next: NextFunction) => {
     try {
         const data = req.body
+        if (data?.thumbnail) {
+            const myCloud = await cloudinary.uploader.upload(data?.thumbnail, {
+                folder: "courses"
+            })
+
+            data.thumbnail = {
+                public_id: myCloud.public_id,
+                url: myCloud.secure_url
+            }
+
+        }
         createCourse(data, res, next)
     } catch (error: any) {
         return next(new ErrorHandler(error.message, 400))
@@ -17,7 +29,17 @@ const editCourse = catchAsyncError(async (req: Request, res: Response, next: Nex
     try {
         const data = req.body
         const courseId = req.params.id
+        if (data?.thumbnail) {
+            await cloudinary.uploader.destroy(data?.thumbnail?.public_id)
+            const myCloud = await cloudinary.uploader.upload(data?.thumbnail, {
+                folder: "courses"
+            })
+            data.thumbnail = {
+                public_id: myCloud.public_id,
+                url: myCloud.secure_url
+            }
 
+        }
         const course = await courseModel.findByIdAndUpdate(courseId, { $set: data }, {
             new: true
         })
