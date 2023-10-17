78c623531d1c8179f452066fb6b495168c81f546
AbtahiHasan <anikidpersonal@gmail.com>
2023-10-17 22:46:58 +0600

    order model done

---
+++ models/order.model.ts
@@ -0,0 +1,27 @@
+import mongoose, { Document, Model, Schema } from "mongoose";
+
+interface IOrder extends Document {
+    order_id: string;
+    user_id: string;
+    payment_info: object;
+}
+
+const orderSchema = new Schema<IOrder>({
+    order_id: {
+        type: String,
+        required: true
+    },
+    user_id: {
+        type: String,
+        required: true
+    },
+    payment_info: {
+        type: Object,
+        // required: true
+    },
+}, { timestamps: true })
+
+
+const orderModel: Model<IOrder> = mongoose.model("order", orderSchema)
+
+export default orderModel
